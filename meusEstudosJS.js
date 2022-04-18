410ed5e49ffff5569daa5755085417a785867a94
Victor Moreira <100440867+Ultimatejazz@users.noreply.github.com>
2022-04-18 08:52:22 -0300

    Update meusEstudosJS.js

--- meusEstudosJS.js
+++ meusEstudosJS.js
@@ -19,3 +19,15 @@ let myCar2 = new Car("Audi", 2019);
 
 console.log(myCar1);
 console.log(myCar2);
+
+// Function Syntax:
+/*
+function name(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+*/
+
+//Function example:
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
